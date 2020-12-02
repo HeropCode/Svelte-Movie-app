e3bf6bbcce48cbc81e2ccbf6ed7bf16793c786f2
ParkYoungWoong <thesecon@gmail.com>
2020-12-02 14:17:41 +0900

    Modify Footer style

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,23 +1,20 @@
 <footer>
   <div>
-    <div>
-      <a href="https://github.com/HeropCode/Svelte-Movie-app">
-        GitHub Repo.
-      </a>
-    </div>
-    <div>
-      <a href="https://heropy.blog/2019/09/29/svelte/">
-        @{new Date().getFullYear()}
-        HEROPY
-      </a>
-    </div>
+    <a href="https://github.com/HeropCode/Svelte-Movie-app">
+      GitHub Repo.
+    </a>
+  </div>
+  <div>
+    <a href="https://heropy.blog/2019/09/29/svelte/">
+      @{new Date().getFullYear()}
+      HEROPY
+    </a>
   </div>
 </footer>
 
 <style lang="scss">
   footer {
     padding: 70px;
-    color: $color--white--10;
     text-align: center;
     a {
       color: $color--white--10;
